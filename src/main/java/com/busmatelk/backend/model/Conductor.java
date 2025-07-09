97c1207ebef2d10d8d0cf3d160546802d0c6bafc
ILakshitha <ilakshitha7921@gmail.com>
2025-07-09 12:41:58 +0530

    conductor sign up creation

---
+++ src/main/java/com/busmatelk/backend/model/Conductor.java
@@ -0,0 +1,24 @@
+package com.busmatelk.backend.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Conductor {
+    @Id
+    @Column(name = "user_id")
+    private UUID userId;
+    private String employee_id;
+    private String assign_operator_id;
+    private String shift_status;
+}
