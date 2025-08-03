2d77189f197cf054750bce57983544946c4e0958
ILakshitha <ilakshitha7921@gmail.com>
2025-08-03 10:25:40 +0530

    get time keeper function created

--- src/main/java/com/busmatelk/backend/repository/TimekeeperRepo.java
+++ src/main/java/com/busmatelk/backend/repository/TimekeeperRepo.java
@@ -2,9 +2,9 @@ package com.busmatelk.backend.repository;
 
 import com.busmatelk.backend.model.Timekeeper;
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
 import java.util.UUID;
 
 public interface TimekeeperRepo extends JpaRepository<Timekeeper, UUID> {
-    Timekeeper findByUser_UserId(UUID userId);
+    Optional<Timekeeper> findByUserUserId(UUID userId);
 }
-
