899c763840285dc616edd429342255e70518f7c3
Kavinda Dewmith <kavindadewmith@gmail.com>
2025-08-23 07:07:03 +0530

    Update CORS configuration to allow all origins

--- src/main/java/com/busmatelk/backend/config/WebConfig.java
+++ src/main/java/com/busmatelk/backend/config/WebConfig.java
@@ -13,7 +13,8 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3000") // frontend URL
+                        //.allowedOrigins("http://localhost:3000") // frontend URL
+                        .allowedOrigins("*") // Tempory CORS allowance for all URLs
                         .allowedMethods("*") // GET, POST, PUT, etc.
                         .allowedHeaders("*")
                         .allowCredentials(true);
